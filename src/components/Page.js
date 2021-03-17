0e85a86f5cdfcacf99e06090242e8111376979a1
Dawid Janiga <dawid.janiga@netguru.com>
2021-03-17 17:58:00 +0100

    feat(index): display basic layout

---
+++ src/components/Page.js
@@ -0,0 +1,33 @@
+import { Layout as AntLayout } from 'antd'
+import styled from 'styled-components'
+const { Header, Content, Footer: AntComponent } = AntLayout
+
+const Logo = styled.h2`
+  color: #ffffff;
+`
+
+const Inner = styled(Content)`
+  min-height: 280px;
+  padding: 24px;
+  background: #fff;
+`
+
+const Layout = styled(AntLayout)`
+  min-height: 100vh;
+`
+
+const Footer = styled(AntComponent)`
+  text-align: center;
+`
+
+export default function Page () {
+  return (
+    <Layout>
+      <Header>
+        <Logo>Wikipedia Map</Logo>
+      </Header>
+      <Inner></Inner>
+      <Footer>Netguru College React</Footer>
+    </Layout>
+  )
+}
